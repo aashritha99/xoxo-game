455e80df050344f6a38dce1ecb3b934eaf667097
Aashritha <anashrita99@gmail.com>
2025-11-11 16:44:19 +0530

    Implement game layout and functionality for Tic Tac Toe

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("hello");
+let boxes=document.querySelectorAll(".box");
+let reset =document.querySelector(".reset");
+let turn=true;
+
+let winning_patterns=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+//event listener
+boxes.forEach((box) => {
+box.addEventListener("click", ()=> {
+   if(turn === true){
+    box.innerText="o"; 
+    turn=false;
+   }
+   else{
+     box.innerText="x"; 
+     turn=true;
+   }
+   box.disabled =true;
+   checkWinner();
+})
+})
+
